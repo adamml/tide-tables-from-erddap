c1c8930db2255df55f7728bd5509a5246acad076
Adam Leadbetter <adamml@users.noreply.github.com>
2019-08-21 16:19:25 +0100

    Update processTides.js

--- js/processTides.js
+++ js/processTides.js
@@ -5,7 +5,19 @@
   var currentTime = new Date();
   document.getElementById("dateHeading").innerText = currentTime.toLocaleDateString(config.location.locale,config.dateOptions);
   
+  console.log(buildErddapUrl(config));
+  
   function concatLocation(config){
     return config.location.town + ", " + config.location.countyOrState + ", " + config.location.country;
   }
+  
+  function buildErddapUrl(config){
+    var tableOrGrid = "tabledap";
+    if(!config.erddapServer.isTabledap){
+      tableOrGrid = "griddap";
+    }
+    
+    return config.erddapServer.baseURL + "/" + tableOrGrid;
+  }
+  
 }(config));
