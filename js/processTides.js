a20611e2a4d8261756776c987fb3029774f0fd8a
Adam Leadbetter <aleadbetter9@gmail.com>
2020-01-29 12:52:46 +0000

    Fix #2

--- js/processTides.js
+++ js/processTides.js
@@ -38,65 +38,76 @@
 		config.erddapServer.location.value + "%22") +
 		'&' + getDateRange();
 	
-	$.getJSON(erddapQuery, function(data){
-			var tideTable;
-			var lastTide;
-			var today = moment().utc().year() + "-" + 
-				("00" + (moment().utc().month()+1)).slice(-2) + "-" + ("00" + (moment().utc().date())).slice(-2);
-			var yesterday = moment().utc().subtract(1, 'days').year() + "-" + 
-				("00" + (moment().utc().subtract(1, 'days').month()+1)).slice(-2) + 
-				"-" + ("00" + moment().utc().subtract(1, 'days').date()).slice(-2);
-			var oddEven = "odd";
-			var materialArrow;
-			data.table.rows.forEach(function(item, index) {
-				if(index > 0){
-					if(index > 1 &&
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', erddapQuery);
+	
+	xhr.onload = function(){
+			
+    if (xhr.status === 200) {
+        data = JSON.parse(xhr.responseText);
+		var tideTable;
+		var lastTide;
+		var today = moment().utc().year() + "-" + 
+			("00" + (moment().utc().month()+1)).slice(-2) + "-" + ("00" + (moment().utc().date())).slice(-2);
+		var yesterday = moment().utc().subtract(1, 'days').year() + "-" + 
+			("00" + (moment().utc().subtract(1, 'days').month()+1)).slice(-2) + 
+			"-" + ("00" + moment().utc().subtract(1, 'days').date()).slice(-2);
+		var oddEven = "odd";
+		var materialArrow;
+		data.table.rows.forEach(function(item, index) {
+			if(index > 0){
+				if(index > 1 &&
 						Math.sign(item[1] - data.table.rows[index-1][1]) != lastTide && 
 						item[1] - data.table.rows[index-1][1] != 0){
-							if(lastTide == 1){
-								materialArrow = '<i class="material-icons">arrow_upward</i>'
-							} else {
-								materialArrow = '<i class="material-icons">arrow_downward</i>'
-							}
-							if(moment().isDST()){
-								if((data.table.rows[index-1][0].search(today) > -1 &&
-									parseInt(data.table.rows[index-1][0].substring(11,13)) < 23) ||
-									(data.table.rows[index-1][0].search(yesterday) > -1) &&
-									parseInt(data.table.rows[index-1][0].substring(11,13)) > 22) {
-										
-										time = parseInt(data.table.rows[index-1][0].substring(11,13))+1;
-										document.getElementById("tideTable").innerHTML = 
-											document.getElementById("tideTable").innerHTML +
-											'<div class="' + oddEven + '">' +
-											'<div class="tideArrow">' + materialArrow + '</div>' +
-											'<div class="tideTime">' + 
-											('00' + time.toString()).slice(-2) + 
-											':' + data.table.rows[index-1][0].substring(14,16) + '</div>' +
-											'<div class="tideHeight">' + data.table.rows[index-1][1] + '</div>' +
-											'</div>';
-										if(oddEven == 'odd'){oddEven = 'even'; } else {oddEven = 'odd'; }
-								}
-							}
-							else{
-								if(data.table.rows[index-1][0].search(today) > -1){
-									document.getElementById("tideTable").innerHTML = 
-										document.getElementById("tideTable").innerHTML +
-										'<div class="' + oddEven + '">' +
-										'<div class="tideArrow">' + materialArrow + '</div>' +
-										'<div class="tideTime">' + data.table.rows[index-1][0].substring(11,16) + '</div>' +
-										'<div class="tideHeight">' + data.table.rows[index-1][1] + '</div>' 
-										'</div>';
-									if(oddEven == 'odd'){oddEven = 'even'; } else {oddEven = 'odd'; }
-								}
-							}
+					if(lastTide == 1){
+						materialArrow = '<i class="material-icons">arrow_upward</i>'
+					} else {
+						materialArrow = '<i class="material-icons">arrow_downward</i>'
 					}
-					if(item[1] - data.table.rows[index-1][1] != 0){
-						lastTide = Math.sign(item[1] - data.table.rows[index-1][1]);
+					if(moment().isDST()){
+						if((data.table.rows[index-1][0].search(today) > -1 &&
+								parseInt(data.table.rows[index-1][0].substring(11,13)) < 23) ||
+								(data.table.rows[index-1][0].search(yesterday) > -1) &&
+								parseInt(data.table.rows[index-1][0].substring(11,13)) > 22) {
+										
+							time = parseInt(data.table.rows[index-1][0].substring(11,13))+1;
+								document.getElementById("tideTable").innerHTML = 
+								document.getElementById("tideTable").innerHTML +
+								'<div class="' + oddEven + '">' +
+								'<div class="tideArrow">' + materialArrow + '</div>' +
+								'<div class="tideTime">' + 
+								('00' + time.toString()).slice(-2) + 
+								':' + data.table.rows[index-1][0].substring(14,16) + '</div>' +
+								'<div class="tideHeight">' + data.table.rows[index-1][1] + '</div>' +
+								'</div>';
+								if(oddEven == 'odd'){oddEven = 'even'; } else {oddEven = 'odd'; }
+						}
+					}
+					else{
+						if(data.table.rows[index-1][0].search(today) > -1){
+							document.getElementById("tideTable").innerHTML = 
+								document.getElementById("tideTable").innerHTML +
+								'<div class="' + oddEven + '">' +
+								'<div class="tideArrow">' + materialArrow + '</div>' +
+								'<div class="tideTime">' + data.table.rows[index-1][0].substring(11,16) + '</div>' +
+								'<div class="tideHeight">' + data.table.rows[index-1][1] + '</div>' 
+								'</div>';
+							if(oddEven == 'odd'){oddEven = 'even'; } else {oddEven = 'odd'; }
+						}
 					}
 				}
-			});
+				if(item[1] - data.table.rows[index-1][1] != 0){
+					lastTide = Math.sign(item[1] - data.table.rows[index-1][1]);
+				}
+			}
 		});
+	}
+    else {
+        console.log('Request failed.  Returned status of ' + xhr.status);
+    }};
+	xhr.send();
   }
+	
   
   function getDateRange(){
 	  moment();
