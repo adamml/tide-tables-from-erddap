2ecf1c605cc712dddace2fe026bfc4f75dbd4b04
Adam Leadbetter <aleadbetter9@gmail.com>
2021-01-06 15:11:20 +0000

    Add Met Eireann credit

--- js/config.js
+++ js/config.js
@@ -20,7 +20,7 @@ const config={
       "height": "sea_surface_height"
     }
   },
-  "acknowledgement": "Data supplied by the [Environmental Protection Agency](https://www.epa.ie/), Ireland and the [Marine Institute](https://www.marine.ie), Ireland.",
+  "acknowledgement": "Data supplied by the [Environmental Protection Agency](https://www.epa.ie/), Ireland; the [Marine Institute](https://www.marine.ie), Ireland and [Met Eireann](https://www.met.ie/), Ireland.",
   "optional": {
 	  "temperature": {
 		  "title": "Water temperature at Spiddal"
